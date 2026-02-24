3a6b1932c59c392fc3dbd06104de4bec030c5b9c
Danilo Macri <danilomacri11@gmail.com>
2026-02-24 08:19:52 -0300

    Update ex3.dart

--- atividades extra/ex3.dart
+++ atividades extra/ex3.dart
@@ -1,6 +1,17 @@
+import "dart:io";
+
 void salarioLiquido(double salarioBruto) {
   double impostos, bonificacao, total;
 
   impostos = salarioBruto * 0.1;
-  bonificacao = salarioBruto * 1.2;
+  bonificacao = salarioBruto * 0.2;
+  total = salarioBruto - impostos + bonificacao;
+  print("Salário total: $total");
+}
+
+void main(){
+  double salario;
+  print("Digite qual seu salário bruto");
+  salario = double.parse(stdin.readLineSync()!);
+  salarioLiquido(salario);
 }
