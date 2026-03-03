e6b64bc912168e1cb5188fc0a8b91fe94e8e6778
Danilo Macri <danilomacri11@gmail.com>
2026-03-02 21:47:23 -0300

    Delete ex12.dart

--- ex12.dart
+++
@@ -1,9 +0,0 @@
-Future<void> carregarDados() async {
-  print("Carregando...");
-  await Future.delayed(Duration(seconds: 2));
-  print("Dados carregados");
-}
-
-void main() async {
-  await carregarDados();
-}
