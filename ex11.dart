ef593e88a5aa00b002418a0901e5f304c270517d
Danilo Macri <danilomacri11@gmail.com>
2026-03-02 21:47:15 -0300

    Delete ex11.dart

--- ex11.dart
+++
@@ -1,9 +0,0 @@
-void criarUsuario(int idade, {required String nome}) {
-  print("Idade: $idade, Usuário: $nome");
-}
-
-var anonimafunc = () => print("Olá Dart!");
-void main() {
-  criarUsuario(30, nome: "Danilo");
-  anonimafunc();
-}
